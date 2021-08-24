707cbdca5f3fb6ade6974cbbd40deab17e7ff9c9
qwbarch <edwardyang0410@gmail.com>
2021-08-23 22:07:06 -0400

    Add the submit and response http endpoints

--- src/TwoCaptcha/Internal/Client.hs
+++ src/TwoCaptcha/Internal/Client.hs
@@ -3,14 +3,18 @@ module TwoCaptcha.Internal.Client where
 import Control.Lens ((^?))
 import Control.Monad.Catch (MonadCatch, MonadThrow (throwM))
 import Control.Monad.IO.Class (MonadIO (liftIO))
+import Control.Monad.Reader.Class (MonadReader (ask))
+import Data.Aeson (Value (Null))
 import Data.Aeson.Lens (key, _Integer, _String)
 import Data.ByteString.Lazy.Char8 (ByteString)
 import Data.Text (Text, unpack)
 import Network.Wreq (Options, Response, responseBody)
+import Network.Wreq.Session (Session, getWith, postWith)
 import TwoCaptcha.Internal.Types.Exception (TwoCaptchaException (TwoCaptchaResponseException, UnknownError), readErrorCode)
 
-request :: (MonadIO m, MonadCatch m) => IO (Response ByteString) -> Options -> m Text
-request method options = do
+-- | Handles the given http request and rethrows exceptions.
+handle :: (MonadIO m, MonadCatch m) => IO (Response ByteString) -> m Text
+handle method = do
   response <- liftIO method
   let statusRequest = do
         body <- response ^? responseBody
@@ -30,3 +34,11 @@ request method options = do
           Right errorCode -> throwM $ TwoCaptchaResponseException errorCode
         else -- No error was found.
           pure request
+
+-- | Submits a captcha to 2captcha's API.
+submit :: (MonadReader Session m, MonadIO m, MonadCatch m) => Options -> m Text
+submit options = ask >>= \session -> handle $ postWith options session "https://2captcha.com/in.php" Null
+
+-- | Retrieves the captcha solution from 2captcha's API.
+response :: (MonadReader Session m, MonadIO m, MonadCatch m) => Options -> m Text
+response options = ask >>= \session -> handle $ getWith options session "https://2captcha.com/res.php"
