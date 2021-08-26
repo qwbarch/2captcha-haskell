e3d214534a164d3d17626f75fc50e917643fdae2
qwbarch <edwardyang0410@gmail.com>
2021-08-26 13:53:29 -0400

    Fix bool lenses storing the incorrect value
    True/false was translated to its literal string value, but 2captcha uses 0/1 as a bool value

--- src/TwoCaptcha/Internal/Types/Captcha.hs
+++ src/TwoCaptcha/Internal/Types/Captcha.hs
@@ -10,8 +10,8 @@ defaultCaptcha :: Options
 defaultCaptcha = defaults & param "json" .~ ["1"]
 
 -- | Creates a lens using the given field name.
-lens' :: Coercible Options a => Text -> Lens' a (Maybe Text)
-lens' field = lens getter setter
+mkLens :: Coercible Options a => Text -> Lens' a (Maybe Text)
+mkLens field = lens getter setter
   where
     getter a =
       if null value
@@ -22,31 +22,50 @@ lens' field = lens getter setter
     setter a (Just value) = coerce $ coerce a & param field .~ [value]
     setter a Nothing = a
 
--- | Create a lens using the given field name for type __b__ with a 'Show' and 'Read' instance.
-lens'' :: (Coercible Options a, Show b, Read b) => Text -> Lens' a (Maybe b)
-lens'' field = lens' field . iso (read . unpack <$>) (pack . show <$>)
+-- |
+-- Create a lens using the given field name for type __b__ with a 'Show' and 'Read' instance.
+--
+-- GOTCHA: Bool values translate to 'True' or 'False'. Use 'mkLensBool' instead for bool lenses.
+mkLens' :: (Coercible Options a, Show b, Read b) => Text -> Lens' a (Maybe b)
+mkLens' field = mkLens field . iso (read . unpack <$>) (pack . show <$>)
+
+-- |
+-- Create a lens using the given field name for bools.
+--
+-- Translation:
+--
+-- * 'True' - 1
+-- * 'False' - 0
+mkLensBool :: Coercible Options a => Text -> Lens' a (Maybe Bool)
+mkLensBool field = mkLens field . iso (textToBool <$>) (boolToText <$>)
+  where
+    textToBool "0" = False
+    textToBool "1" = True
+    textToBool number = error $ unpack number <> " is not a valid bool value. This should never occur, please don't manipulate the options manually."
+    boolToText False = "0"
+    boolToText True = "1"
 
 -- | Lenses for constructing captcha options.
 class Coercible Options a => CaptchaLike a where
   -- | Your 2captcha API <https://2captcha.com/2captcha-api#solving_captchas key>.
   apiKey :: Lens' a (Maybe Text)
-  apiKey = lens' "key"
+  apiKey = mkLens "key"
 
   -- | Software developer id. Developers who integrate their software with 2captcha earn 10% of the user's spendings.
   softId :: Lens' a (Maybe Int)
-  softId = lens'' "soft_id"
+  softId = mkLens' "soft_id"
 
   -- | URL for <https://2captcha.com/2captcha-api#pingback pingback> (callback) response that will be sent the answer to when the captcha is solved.
   pingback :: Lens' a (Maybe Text)
-  pingback = lens' "pingback"
+  pingback = mkLens "pingback"
 
   -- |
   -- Proxy to be sent to the worker who solves the captcha. You can read more about proxies <https://2captcha.com/2captcha-api#proxies here>.
   --
   -- Format must be in __login:password@123.123.123.123:3128__ .
   proxy :: Lens' a (Maybe Text)
-  proxy = lens' "proxy"
+  proxy = mkLens "proxy"
 
   -- | Type of your proxy: __HTTP__, __HTTPS__, __SOCKS4__, __SOCKS5__.
   proxyType :: Lens' a (Maybe Text)
-  proxyType = lens' "proxytype"
+  proxyType = mkLens "proxytype"
--- src/TwoCaptcha/Internal/Types/ReCaptcha.hs
+++ src/TwoCaptcha/Internal/Types/ReCaptcha.hs
@@ -5,7 +5,7 @@ import Data.Text (Text, pack, unpack)
 import GHC.Base (Coercible)
 import Network.Wreq (Options)
 import Network.Wreq.Lens (param)
-import TwoCaptcha.Internal.Types.Captcha (CaptchaLike, defaultCaptcha, lens', lens'')
+import TwoCaptcha.Internal.Types.Captcha (CaptchaLike, defaultCaptcha, mkLens, mkLens', mkLensBool)
 
 -- | Default options for a reCAPTCHA.
 defaultReCaptcha :: Options
@@ -15,28 +15,28 @@ defaultReCaptcha = defaultCaptcha & param "method" .~ ["userrecaptcha"]
 class Coercible Options a => ReCaptchaLike a where
   -- | Defines if your ReCaptcha is enterprise.
   enterprise :: Lens' a (Maybe Bool)
-  enterprise = lens'' "enterprise"
+  enterprise = mkLensBool "enterprise"
 
   -- |
   -- If using 'ReCaptchaV2', this is the value of __k__ or __data-sitekey__ found on the captcha page.
   --
   -- If using 'ReCaptchaV3', this is the value of __sitekey__ found on the captcha page.
   googleKey :: Lens' a (Maybe Text)
-  googleKey = lens' "googlekey"
+  googleKey = mkLens "googlekey"
 
   -- | Full URL of the page where the reCAPTCHA is found.
   pageUrl :: Lens' a (Maybe Text)
-  pageUrl = lens' "pageurl"
+  pageUrl = mkLens "pageurl"
 
   -- | Domain used to load the captcha: __google.com__ or __recaptcha.net__.
   domain :: Lens' a (Maybe Text)
-  domain = lens' "domain"
+  domain = mkLens "domain"
 
   -- |
   -- If 'True', __in.php__ will include the __Access-Control-Allow-Origin:*__ header in the response.
   -- Used for cross-domain AJAX requests in web applications.
   headerACAO :: Lens' a (Maybe Int)
-  headerACAO = lens'' "header_acao"
+  headerACAO = mkLens' "header_acao"
 
 -- | Parameters used to solve reCAPTCHA V2.
 newtype ReCaptchaV2 = MkReCaptchaV2 Options deriving (Show)
@@ -68,26 +68,26 @@ instance ReCaptchaLike ReCaptchaV2
 -- * 'TwoCaptcha.Internal.Types.Captcha.proxy'
 -- * 'TwoCaptcha.Internal.Types.Captcha.proxyType'
 reCAPTCHAV2 :: ReCaptchaV2
-reCAPTCHAV2 = MkReCaptchaV2 $ defaultReCaptcha
+reCAPTCHAV2 = MkReCaptchaV2 defaultReCaptcha
 
 -- | Defines if the reCAPTCHA v2 is invisible.
 invisible :: Lens' ReCaptchaV2 (Maybe Bool)
-invisible = lens' "invisible" . iso (read . unpack <$>) (pack . show <$>)
+invisible = mkLensBool "invisible"
 
 -- | Value of the __data-s__ parameter found on the reCAPTCHA page. Currently applicable for google services.
 dataS :: Lens' ReCaptchaV2 (Maybe Text)
-dataS = lens' "data-s"
+dataS = mkLens "data-s"
 
 -- |
 -- Cookies that will be used by the worker solving the reCAPTCHA. The used cookies will also be included in the response.
 --
 -- Format: __KEY1:Value1;KEY2:Value2;__
 cookies :: Lens' ReCaptchaV2 (Maybe Text)
-cookies = lens' "cookies"
+cookies = mkLens "cookies"
 
 -- | User agent that will be used by the worker solving the reCAPTCHA.
 userAgent :: Lens' ReCaptchaV2 (Maybe Text)
-userAgent = lens' "userAgent"
+userAgent = mkLens "userAgent"
 
 -- | Parameters used to solve reCAPTCHA V3.
 newtype ReCaptchaV3 = MkReCaptchaV3 Options deriving (Show)
@@ -98,4 +98,4 @@ instance ReCaptchaLike ReCaptchaV3
 
 -- | The score needed for resolution. Currently it's almost impossible to get a token with a score higher than 0.3
 minScore :: Lens' ReCaptchaV3 (Maybe Double)
-minScore = lens'' "min_score"
+minScore = mkLens' "min_score"
